7cae8ac537a88ce8c45400d2fe6c983ba3955445
thinhkieu725 <thinhkieu725@gmail.com>
2024-01-28 14:17:28 +0200

    Add read_payload()

--- Delivery_fee_calculator.py
+++ Delivery_fee_calculator.py
@@ -1,18 +1,31 @@
 """
+This is the Backend specifics part of the Wolt's trainee assignment.
+This project aims to calculate the delivery fee.
 
-
+The following file(s) must be in the same working directory:
+request.json
 
 Creator: Thinh Kieu
-Student ID number: 152167613
-Student ID: dxthki
 Email: thinhkieu726@gmail.com
-
-#TODO: Write program description.
 """
+
+import json
+
+
+def read_payload(file_name):
+    """
+    Get the .json file name and return the content as a dictionary.
+    :param file_name: str : name of the file to be read
+    :return: dict : the content of the file
+    """
+    with open("request.json", "r") as file_name:
+        content = json.load(file_name)
+        return content
 
 
 def main():
-    pass
+    # Read the request payload
+    request = read_payload("request.json")
 
 
 if __name__ == "__main__":
