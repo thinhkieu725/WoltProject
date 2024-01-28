6c5afc058911c959aa8d53b837408f3319029f0f
thinhkieu725 <thinhkieu725@gmail.com>
2024-01-28 13:27:46 +0200

    Create file

---
+++ Delivery_fee_calculator.py
@@ -0,0 +1,19 @@
+"""
+
+
+
+Creator: Thinh Kieu
+Student ID number: 152167613
+Student ID: dxthki
+Email: thinhkieu726@gmail.com
+
+#TODO: Write program description.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
